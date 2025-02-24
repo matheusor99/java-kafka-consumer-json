f6515e17a24e097577c75d295770ca1f82a244e1
matheusor99 <matheusor99@gmail.com>
2025-02-24 20:20:30 -0300

    project kafka

---
+++ src/main/java/com/matheusor99/java_kafka_consumer_json/JavaKafkaConsumerJsonApplication.java
@@ -0,0 +1,13 @@
+package com.matheusor99.java_kafka_consumer_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaConsumerJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaConsumerJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusor99/java_kafka_consumer_json/JavaKafkaConsumerJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusor99.java_kafka_consumer_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaConsumerJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
